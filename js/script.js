a581fb5ed88554ffa0eaef15e747f26d65e7820a
Marcin Tyminski <tymin12@o2.pl>
2021-05-24 14:46:07 +0200

    Add class active to clicked link

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const titleClickHandler = function(event) {
     console.log('Link was clicked!');
     console.log(event)
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -12,15 +12,21 @@ const titleClickHandler = function(event) {
         activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
+
+    const clickedElement = this;
+
+    clickedElement.classList.add('active')
+
+    console.log('clickedElement:', clickedElement);
+
+    /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.posts article.active');
 
     for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
-
-    /* remove class 'active' from all articles */
 
     /* get 'href' attribute from the clicked link */
 
